7a956a6093e56746ec0e71f4cc8bf2c2e05e5b14
Aman <amanhusain8899@gmail.com>
2026-07-20 21:29:25 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Swiggy deleivery time prediction project',
+    author='Aman',
+    license='MIT',
+)
